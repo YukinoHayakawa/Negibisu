d430ef314d70e98a76d9811dc75b57d27486816c
Yukino Hayakawa <tennencoll@gmail.com>
2019-05-30 17:56:56 +0800

    init

---
+++ main.cpp
@@ -0,0 +1,18 @@
+﻿#include <Usagi/Runtime/Runtime.hpp>
+
+
+using namespace usagi;
+
+int main(int argc, char *argv[])
+{
+	auto runtime = Runtime::create();
+	runtime->enableCrashHandler("GAMECrashDump");
+	try
+	{
+	}
+	catch(const std::exception &e)
+	{
+		runtime->displayErrorDialog(e.what());
+		throw;
+	}
+}
